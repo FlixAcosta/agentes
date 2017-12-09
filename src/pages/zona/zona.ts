6cef1b35b7c0d3d9adcce78c797f0a4e6fca1781
Felix <stop.pfr@gmail.com>
2017-12-09 00:49:44 -0600

    Google Maps funcionando, Enfoque Durango

--- src/pages/zona/zona.ts
+++ src/pages/zona/zona.ts
@@ -6,6 +6,7 @@ import {
   GoogleMapsEvent,
   GoogleMapOptions
  } from '@ionic-native/google-maps';
+import { ChequeoPage } from '../chequeo/chequeo';
  
 
 
@@ -16,18 +17,26 @@ import {
 export class ZonaPage {
 
   map: GoogleMap;
+ 
+ 
 
   constructor(public navCtrl: NavController,
      public navParams: NavParams,
-     private googleMaps: GoogleMaps) {
-  }
+     private googleMaps: GoogleMaps) {}
+
+
+  ionViewDidLoad() {
+    console.log("zona");
+    this.loadMap();
+    
+  } 
   loadMap() {
     
         let mapOptions: GoogleMapOptions = {
           camera: {
             target: {
-              lat: 43.0741904,
-              lng: -89.3809802
+              lat: 24.02772,
+              lng: -104.653176
             },
             zoom: 18,
             tilt: 30
@@ -43,22 +52,26 @@ export class ZonaPage {
     
             // Now you can use all methods safely.
             this.map.addMarker({
-                title: 'Ionic',
+                title: 'Zona 1',
                 icon: 'blue',
                 animation: 'DROP',
                 position: {
-                  lat: 43.0741904,
-                  lng: -89.3809802
+                  lat: 24.02772,
+                  lng: -104.653176
                 }
               })
               .then(marker => {
                 marker.on(GoogleMapsEvent.MARKER_CLICK)
                   .subscribe(() => {
-                    alert('clicked');
+                    alert('Comienza el chequeo de parquimetros en la zona');
+                    this.irAchequeo();
                   });
               });
     
           });
-      } 
+      }
 
+      irAchequeo(){
+        this.navCtrl.push(ChequeoPage);
+      }
 }
